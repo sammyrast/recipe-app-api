ae79d3b428aba225ef5706acb866dffcd4bde444
SamsonDemma <samson.demma@eurowag.com>
2022-10-19 09:29:57 +0200

    normalized emails

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,6 @@
 """
 Database models.
 """
-from email.policy import default
-from enum import unique
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -16,7 +14,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_field):
         """Create , svae and return a new user."""
-        user = self.model(email=email, **extra_field)
+        user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self._db)
 
