fc5dd1295000219046dc014d0c282af277aa475d
SamsonDemma <samson.demma@eurowag.com>
2022-10-19 09:33:17 +0200

    add value error capturing

--- app/core/models.py
+++ app/core/models.py
@@ -15,6 +15,8 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_field):
         """Create , svae and return a new user."""
         user = self.model(email=self.normalize_email(email), **extra_field)
+        if not email:
+            raise ValueError('User must have an email address.')
         user.set_password(password)
         user.save(using=self._db)
 
